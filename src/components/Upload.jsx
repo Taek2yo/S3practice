e1b614e7700de0fe71da6fbdf72086edcb8b22e2
Mintaek2 <789rkd@naver.com>
2022-11-07 21:16:04 +0900

    chore : Router 설정

---
+++ src/components/Upload.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Upload = () =>{
+    return (
+        <>
+            <div>안녕하세요</div>
+        </>
+    )
+}
+
+export default Upload;
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Upload from "../components/Upload";
+
+const MainPage = ()=>{
+
+    return(
+        <>
+            <Upload/>
+        </>
+    )
+}
+
+export default MainPage
