e18ef572311c4c47d9ba071d9d4e864763f98fe7
Mintaek2 <789rkd@naver.com>
2022-11-07 23:09:43 +0900

    chore : aws-sdk 추가

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import AWS from 'aws-sdk';
 
 const Upload = () =>{
     return (
-        <>
-            <div>안녕하세요</div>
-        </>
+        <Container>
+            <img src="" alt=""/>
+            <input type={"file"}></input>
+        </Container>
     )
 }
 
 export default Upload;
+
+const Container = styled.div`
+    align-items: center;
+    text-align: center;
+`
